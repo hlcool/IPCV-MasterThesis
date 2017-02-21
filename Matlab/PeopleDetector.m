0cd6a9c71b87afe5cb74d66067a84ecaae66e785
Alex Lopez <alexlopezcifuentes.93@gmail.com>
2017-02-21 11:06:26 +0100

    Matlab PeopleDetector file modified

--- Matlab/PeopleDetector.m
+++ Matlab/PeopleDetector.m
@@ -5,7 +5,7 @@ close all;
 
 % Parameters
 VideoName = 'HallCutted.mpg';
-Method = 'accurate';
+Method = 'fast';
 
 % Videofile Structure
 VideoFile = VideoReader(VideoName);
@@ -15,23 +15,28 @@ fid = fopen( [VideoName(1:strfind(VideoName, '.') - 1) Method '.txt'], 'wt' );
 FrameNumber = 1;
 
 while hasFrame(VideoFile)
-    tic
     
     Image = readFrame(VideoFile);
     Image = imresize(Image, 2, 'lanczos3');
     Image = imgaussfilt(Image, 1);
     
+    tic
     [ Proposals ] = ProposalExtractor( Image, Method );
+    time1 = toc;
+    disp(['Elapsed time for proposal extraction is: ' num2str(time1)]) 
     
     % FAST_ RCNN
     % [x1, y1, x2, y2]
+    tic
     Detections = fast_rcnn_mod( Image, Proposals - 1 );
+    time2 = toc;
+    disp(['Elapsed time for people detection is: ' num2str(time2)]) 
     
     % Export the bounding boxes from Frame to a txt file
     ExportDetections( fid, Detections, FrameNumber );
     
     disp(['Computed frame ' num2str(FrameNumber) '/' num2str(round(VideoFile.Duration * VideoFile.FrameRate))...
-        ' in ' num2str(toc) ' seconds.']);
+        ' in ' num2str(time1 + time2) ' seconds.']);
     
     FrameNumber = FrameNumber + 1;
 end
