7f01978f8d066cde300b33bac5f311bd5e2229f5
Alex Lopez <alexlopezcifuentes.93@gmail.com>
2017-04-03 11:26:54 +0200

    New temporal merging files

--- Matlab/TemporalMergingSemanticMean.m
+++ Matlab/TemporalMergingSemanticMean.m
@@ -3,41 +3,34 @@ clear all;
 close all;
 
 tic
-for CameraNumber = 1 : 3
+for CameraNumber = 1:3
     % Windows
-    %sempath = ['F:\TFM Videos\Sincronizados\Recording 2\Semantic\Camera ' num2str(CameraNumber) '\'];
+    sempath = ['F:\TFM Videos\Sincronizados\Recording 2\Semantic\Camera ' num2str(CameraNumber) '\'];
     
     load('objectName150.mat');
     listingSemantic = dir(sempath);
     
-    NImages = size(listingSemantic,1);
-    FinalTemporalLabels = zeros(480,640);
+    NFiles = size(listingSemantic,1);
+    FinalTemporalLabels = zeros(480,640, size(objectNames,1));
     
-    Counter = 1;
-    for imageIndex = 1 : NImages
-        
-        disp(['Processing Frame ' num2str(imageIndex) '/' num2str(NImages)])
+    NImages = 1;
+    for imageIndex = 1 : NFiles
         semname = listingSemantic(imageIndex).name;
-        
         if listingSemantic(imageIndex).bytes > 100
-            
+            disp(['Processing Frame ' num2str(NImages) '/' num2str(NFiles)])
             load([sempath,semname]);
             for j = 1 : size(objectNames,1)
-                
-                FinalTemporalLabels(:,:,j) = FinalTemporalLabels(:,:,j) + data(:,:,j);
-                
+                FinalTemporalLabels(:,:,j) = FinalTemporalLabels(:,:,j) + data(:,:,j); 
             end
-            
+            NImages = NImages + 1;
         end
     end
     
     for j = 1 : size(objectNames,1)
-        
         FinalTemporalLabels(:,:,j) = FinalTemporalLabels(:,:,j) ./ NImages;
-        
     end
     
-    FinalTemporalLabels = max(FinalTemporalLabels,3);
+    [FinalTemporalScores,FinalTemporalLabels] = max(data, [], 3);
     
     exportName = ['Camera ' num2str(CameraNumber) '.png'];
     imwrite(uint8(FinalTemporalLabels),exportName);
