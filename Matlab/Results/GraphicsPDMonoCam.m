3438321bd95326229a3abb614ca23c638a46ea29
Alex Lopez <alexlopezcifuentes.93@gmail.com>
2017-06-19 11:31:10 +0200

    Report changes and results update

--- Matlab/Results/GraphicsPDMonoCam.m
+++ Matlab/Results/GraphicsPDMonoCam.m
@@ -11,15 +11,15 @@ close all;
 HOGPrecision1 = [0.58341 0.71336 0.884319 0.957447 1];
 HOGRecall1 = [0.16909 0.1332 0.0459463 0.0060104 0.00013356];
 
-DPMPrecision1 = [0.0140929];
-DPMRecall1 = [0.241007];
+DPMPrecision1 = [0.0140929 0.0182355 0.0681893 0.633588 0.964497];
+DPMRecall1 = [0.241007 0.257361 0.328032 0.265777 0.108855];
 
 figure(1)
-plot(1 - HOGPrecision1, HOGRecall1, '-rx')
+plot(HOGPrecision1, HOGRecall1, '-rx')
 hold on;
-plot(1 - DPMPrecision1, DPMRecall1, '-b*')
+plot(DPMPrecision1, DPMRecall1, '-b*')
 xlim([0 1]);ylim([0 1]);
-xlabel('1 - Precision');
+xlabel('Precision');
 ylabel('Recall');
 legend('HOG','DPM')
 title('Camera 1 Pedestrian Detection Mono Camera')
@@ -28,22 +28,32 @@ title('Camera 1 Pedestrian Detection Mono Camera')
 HOGPrecision2 = [0.18009 0.30972 0.407166 0.459016 0.75];
 HOGRecall2 = [0.09540 0.0610 0.0202823 0.0045432 0.00048677];
 
+DPMPrecision2 = [0.0105133 0.0154302 0.0536681 0.158117 0.840299];
+DPMRecall2 = [0.205613 0.242754 0.303318 0.243432 0.091351];
+
 figure(2)
-plot(1 - HOGPrecision2, HOGRecall2, '-rx')
+plot(HOGPrecision2, HOGRecall2, '-rx')
+hold on;
+plot(DPMPrecision2, DPMRecall2, '-b*')
 xlim([0 1]);ylim([0 1]);
-xlabel('1 - Precision');
+xlabel('Precision');
 ylabel('Recall');
-legend('HOG')
+legend('HOG','DPM')
 title('Camera 2 Pedestrian Detection Mono Camera')
 
 % Camera 3
 HOGPrecision3 = [0.35194 0.50976 0.689038 0.742188  0.92];
 HOGRecall3 = [0.23911 0.1861 0.0784114 0.0241853 0.00585987];
 
+DPMPrecision3 = [0.0120268 0.0161443 0.0603696 0.218275 0.611303];
+DPMRecall3 = [0.38032 0.399751 0.492798 0.456605 0.135032];
+
 figure(3)
-plot(1 - HOGPrecision3, HOGRecall3, '-rx')
+plot(HOGPrecision3, HOGRecall3, '-rx')
+hold on;
+plot(DPMPrecision3, DPMRecall3, '-b*')
 xlim([0 1]);ylim([0 1]);
-xlabel('1 - Precision');
+xlabel('Precision');
 ylabel('Recall');
-legend('HOG')
+legend('HOG','DPM')
 title('Camera 3 Pedestrian Detection Mono Camera')
---
+++ Matlab/Results/GraphicsPDSemantic.m
@@ -0,0 +1,59 @@
+clc;
+clear all;
+close all;
+
+
+%% Pedestrian Detection Mono Camera
+
+% HOG
+
+% Camera 1
+HOGPrecision1 = [0.500374 0.620092 0.85214 0.944444 1];
+HOGRecall1 = [0.0893549 0.0717243 0.0292507 0.0045412 0.000133565];
+
+DPMPrecision1 = [0.028032 0.0294195 0.069287 0.633588 0.992764];
+DPMRecall1 = [0.154541 0.162377 0.210086 0.265777 0.0916255];
+
+figure(1)
+plot(HOGPrecision1, HOGRecall1, '-rx')
+hold on;
+plot(DPMPrecision1, DPMRecall1, '-b*')
+xlim([0 1]);ylim([0 1]);
+xlabel('Precision');
+ylabel('Recall');
+legend('HOG','DPM')
+title('Camera 1 Pedestrian Detection Mono Camera with semantic constraint')
+
+% Camera 2
+HOGPrecision2 = [0.223342 0.402685 0.677966 0.756757 1 ];
+HOGRecall2 = [0.0863216 0.0584131 0.019471 0.0045432 0.000486776];
+
+DPMPrecision2 = [0.0200525 0.0234217 0.0512351 0.158117 0.834101];
+DPMRecall2 = [0.159186 0.186966 0.248346 0.243432 0.0881064];
+
+figure(2)
+plot(HOGPrecision2, HOGRecall2, '-rx')
+hold on;
+plot(DPMPrecision2, DPMRecall2, '-b*')
+xlim([0 1]);ylim([0 1]);
+xlabel('Precision');
+ylabel('Recall');
+legend('HOG','DPM')
+title('Camera 2 Pedestrian Detection Mono Camera with semantic constraint')
+
+% Camera 3
+HOGPrecision3 = [0.327098 0.466068 0.681648 0.848485 1];
+HOGRecall3 = [0.164799 0.118981 0.0463694 0.0142675 0.0033121];
+
+DPMPrecision3 = [0.0233628 0.0251848 0.0646747 0.218275 0.664495];
+DPMRecall3 = [0.275329 0.293673 0.390808 0.456605 0.10387];
+
+figure(3)
+plot(HOGPrecision3, HOGRecall3, '-rx')
+hold on;
+plot(DPMPrecision3, DPMRecall3, '-b*')
+xlim([0 1]);ylim([0 1]);
+xlabel('Precision');
+ylabel('Recall');
+legend('HOG','DPM')
+title('Camera 3 Pedestrian Detection Mono Camera with semantic constraint')
